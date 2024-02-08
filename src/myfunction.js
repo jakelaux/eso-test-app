a5559aa694b89a04b9a5994883646a3ce7e6bf28
Jacob Laux <jake@esolutionsone.com>
2024-02-08 13:24:00 -0600

    Updating test

--- src/myfunction.js
+++ src/myfunction.js
@@ -1,6 +1,9 @@
 import { gs } from '@servicenow/glide'
 
-export const showStatusUpdate = function (newStatus, oldStatus) {
+/*export const showStatusUpdate = function (newStatus, oldStatus) {
     gs.addInfoMessage(`Record updated. Old Status: ${oldStatus}`)
     gs.addInfoMessage(`New Status: ${newStatus}`)
+}*/
+export const testFunction = function () {
+    gs.addInfoMessage('testing123 > coming from our sdk test');
 }
--- src/testfunction.js
+++
@@ -1,5 +0,0 @@
-import { gs } from '@servicenow/glide'
-
-export const testFunction = function () {
-    gs.addInfoMessage('testing testing 123')
-}
