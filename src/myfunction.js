4f9807bc369cfa8f53c35efa7324db6db8474536
Jacob Laux <jake@esolutionsone.com>
2024-02-12 11:05:59 -0600

    Added moment as dependency and updating to test out leveraging external library in BR in SN Platform

--- src/myfunction.js
+++ src/myfunction.js
@@ -1,9 +1,14 @@
 import { gs } from '@servicenow/glide'
+import moment from 'moment'
 
 /*export const showStatusUpdate = function (newStatus, oldStatus) {
     gs.addInfoMessage(`Record updated. Old Status: ${oldStatus}`)
     gs.addInfoMessage(`New Status: ${newStatus}`)
 }*/
-export const testFunction = function (testValue) {
-    gs.addInfoMessage(`testing123 > coming from our sdk test ${testValue}`)
+export const testFunction = function (testValue, dateValue) {
+    var a = moment('2024-02-12'); 
+    var b = a.clone().add(1, 'week'); 
+    a.format();
+    b.format();
+    gs.addInfoMessage(`testing123 > coming from our sdk test ${ testValue }  >> and now here's the date value from the todo ${ dateValue } >> value of date a ${ a } -- value of date b (should be a week later) ${ b }`)
 }
