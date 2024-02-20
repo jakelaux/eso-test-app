3d9f90afbe80808820ccc471383830aeec86ebe2
Jacob Laux <jake@esolutionsone.com>
2024-02-20 13:33:29 -0600

    update after deploy

--- src/myfunction.ts
+++ src/myfunction.ts
@@ -1,6 +1,7 @@
-import { GlideRecord } from '@servicenow/glide'
+import { GlideRecord, gs } from '@servicenow/glide'
 import { TestingLogFunction } from './utility/utility'
 
 export const testFunction = function (current: GlideRecord, _prev: GlideRecord) {
+    gs.addInfoMessage('testing in the myfunction: hello!');
     TestingLogFunction(current.getValue('deadline'), current.getValue('test_field'));
 }
