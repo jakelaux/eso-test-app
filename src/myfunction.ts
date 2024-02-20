978b195c742998b63a0b53c8e9b452bfc2ae7888
Jacob Laux <jake@esolutionsone.com>
2024-02-20 13:47:47 -0600

    debugging .ts issues

--- src/myfunction.ts
+++ src/myfunction.ts
@@ -1,7 +1,7 @@
 import { GlideRecord, gs } from '@servicenow/glide'
-import { TestingLogFunction } from './utility/utility'
+//import { TestingLogFunction } from './utility/utility'
 
 export const testFunction = function (current: GlideRecord, _prev: GlideRecord) {
     gs.addInfoMessage('testing in the myfunction: hello!');
-    TestingLogFunction(current.getValue('deadline'), current.getValue('test_field'));
+    //TestingLogFunction(current.getValue('deadline'), current.getValue('test_field'));
 }
